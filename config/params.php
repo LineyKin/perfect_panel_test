2314217717f7ebf7907b010389cd48beb80bfc8f
sam <s.yablokov@telecontact.ru>
2025-03-16 23:36:34 +0300

    токен авторизации теперь хранится в .env

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'fixedToken' => 'your_fixed_token_here_64_characters_long',
+    'fixedToken' => $_ENV['API_TOKEN'],
 ];
